7d8ec70e1a11dac0c654b11bd1ac3a939ba0ac94
Zentozzz <funikov159@gmail.com>
2022-06-17 00:50:30 +0300

    Готовый проект

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './Style/App.scss';
-import './Style/active.scss';
-import './Style/noActive.scss';
+import "./Style/App.scss";
+import "./Style/active.scss";
+import "./Style/noActive.scss";
+import "./Style/disable.scss";
 
-import List from './list.jsx';
-import cat from './assets/cat.json';
+import List from "./list.jsx";
+import cat from "./assets/cat.json";
 
 function App() {
   return (
@@ -14,11 +15,15 @@ function App() {
       <div className="list">
         {cat.map((obj) => (
           <List
+            key={obj.id}
             name={obj.name}
             size={obj.size}
             gift={obj.gift}
             gift_text={obj.gift_text}
             massa_text={obj.massa_text}
+            text_buttom={obj.text_buttom}
+            disable={obj.disable}
+            disable_text={obj.dasable_text}
           />
         ))}
       </div>
--- src/list.jsx
+++ src/list.jsx
@@ -1,39 +1,79 @@
-import React from 'react';
+import React from "react";
 
-import CatPhoto from './assets/img/Cat.png';
+import CatPhoto from "./assets/img/Cat.png";
 
-function List({ name, size, gift, gift_text, massa_text }) {
+function List({
+  name,
+  size,
+  gift,
+  gift_text,
+  massa_text,
+  text_buttom,
+  disable,
+  disable_text,
+}) {
   const [selected, setSelected] = React.useState(false);
+  const [isShown, setIsShown] = React.useState(false);
+
+  function Textbuttom() {
+    if (disable === true) {
+      return <span>{disable_text}</span>;
+    } else if (selected === false) {
+      return (
+        <span>
+          Чего сидишь? Порадуй котэ,&nbsp;
+          <p onClick={() => setSelected(!selected)}>купи.</p>
+        </span>
+      );
+    } else {
+      return <span>{text_buttom}</span>;
+    }
+  }
 
   return (
-    <div className="text_buttom">
-      <div onClick={() => setSelected(!selected)} className={selected ? 'card_active' : 'card'}>
-        <div className="card_path">
-          <div className="text">
-            <p>Сказочное заморское яство</p>
-            <p className="nam">Нямушка</p>
-            <p className="name">{name}</p>
-            <p className="size">
-              <b>{size}</b> порций
-            </p>
-            <p className="gift">
-              <b>{gift}</b> {gift_text}
-            </p>
-          </div>
-          <div className="cat">
-            <img src={CatPhoto} alt="Cat" />
-          </div>
-          <div className="ara">
-            <div className="massa">
-              <p className="massa_text">{massa_text}</p>
-              <p>кг</p>
+    <div className={disable ? "disable" : ""}>
+      <div
+        className="wrapper"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+      >
+        <div
+          onClick={
+            disable === false
+              ? () => setSelected(!selected)
+              : () => setSelected(selected)
+          }
+          className={selected ? "card_active" : "card"}
+        >
+          <div className="card_path">
+            <div className="text">
+              <p id={isShown ? "color" : ""}>
+                {selected === true && isShown === true
+                  ? "Котэ не одобряет?"
+                  : "Сказочное заморское яство"}
+              </p>
+              <p className="nam">Нямушка</p>
+              <p className="name">{name}</p>
+              <p className="size">
+                <b>{size}</b> порций
+              </p>
+              <p className="gift">
+                <b>{gift}</b> {gift_text}
+              </p>
+            </div>
+            <div className="cat">
+              <img src={CatPhoto} alt="Cat" />
+            </div>
+            <div className="ara">
+              <div className="massa">
+                <p className="massa_text">{massa_text}</p>
+                <p>кг</p>
+              </div>
             </div>
           </div>
         </div>
+        <Textbuttom />
       </div>
-      <span>
-        Чего сидишь? Порадуй котэ,&nbsp;<p onClick={() => setSelected(!selected)}>купи.</p>
-      </span>
     </div>
   );
 }
